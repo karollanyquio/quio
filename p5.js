3876eb1da4c6b159aeea183bcf85e5df98348fc1
karollanyquio <0000108647031xsp@al.educacao.sp.gov.br>
2024-06-10 11:33:07 -0300

    Create p5.js

---
+++ p5.js
@@ -0,0 +1,11 @@
+let xbolinha = 100;
+let ybolinha = 100;
+let diamentro = 22;
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(0);
+  circle(xbolinha, ybolinha, diametro);
+}
